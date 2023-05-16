6025303ea06c1ce371477009c872bdcacb1736b7
RyugaXhypeR <ryugashyper@gmail.com>
2023-05-16 17:29:57 +0530

    Addition: `git.lua` helper module to run git commands in a new process

---
+++ lua/github_link/git.lua
@@ -0,0 +1,58 @@
+-- Module to run various git commands in a seperate process and return the output.
+
+local Job = require("plenary.job")
+local M = {}
+
+-- Run git commands in a new process and return the output.
+-- 
+-- Parameters
+-- ==========
+-- args: list
+--   The arguments to pass to git.
+-- 
+-- cwd: str | nil
+--   The working directory in which the command is to be executed.
+--   Defaults to working directory of the buffer if not specified.
+M.git_cmd = function(args, cwd)
+    return Job:new({
+        command = "git",
+        args = args,
+        cwd = cwd or vim.fn.expand("%:p:h"),
+        on_exit = function(j_self, _, _)
+            return table.concat(j_self:result(), "")
+        end,
+    }):sync()
+end
+
+-- Get the current branch name, this will be used to generate the blob for that branch.
+M.get_git_branch = function(cwd)
+    return M.git_cmd({ "branch", "--show-current" }, cwd)
+end
+
+-- Get the current git remote url.
+M.get_repo_url = function(cwd)
+    local url = M.git_cmd({ "config", "--get", "remote.origin.url" }, cwd)
+    if url:find("github") then
+        return url
+    end
+end
+
+-- Get the root directory of the git repo.
+M.get_git_root = function(cwd)
+    return M.git_cmd({ "rev-parse", "--show-toplevel" }, cwd)
+end
+
+-- Get the relative path of the file from the git root.
+--
+-- Parameters
+-- ==========
+-- file_path: str
+--  The path of the file relative to the working directory.
+-- 
+-- cwd: str | nil
+-- The working directory in which the command is to be executed.
+M.get_relative_path = function(file_path, cwd)
+    return M.git_cmd({ "ls-files", "--full-name", "--", file_path }, cwd)
+end
+
+return M
