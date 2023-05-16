5b55f39dcc3fe8210556a06d8f514ca752515e3b
RyugaXhypeR <ryugashyper@gmail.com>
2023-05-16 20:25:57 +0530

    Change: checking if `split`, `branch` and `relative_path` are nil, and docs!.

--- lua/github_link/url_format.lua
+++ lua/github_link/url_format.lua
@@ -1,18 +1,26 @@
 local M = {}
 
+-- Split the url into its components.
 M.url_split = function(url)
   local protocol, domain, user, repo = url:match("([^:@]+).*[/@]([%w_]+).com[:/]([^/]+)/([^/]+).git")
 
-  return {
-    protocol = protocol,
-    domain = domain,
-    user = user,
-    repo = repo,
-  }
+  if protocol and domain and user and repo then
+    return {
+      protocol = protocol,
+      domain = domain,
+      user = user,
+      repo = repo,
+    }
+  end
 end
 
+-- Return the permalink.
 M.url_format = function(repo_url, branch, relative_path, ln_start, ln_stop)
   local split = M.url_split(repo_url)
+  if not (split and branch and relative_path) then
+    return nil
+  end
+
   local blob = ("https://%s.com/%s/%s/blob/%s/%s"):format(
     split.domain, split.user, split.repo, branch, relative_path
   )
