d267b2f4dada3f777e7264d6449cf4607a551463
RyugaXhypeR <ryugashyper@gmail.com>
2023-05-17 08:35:00 +0530

    Fix: removed hardcoding `remote.origin`.
    Fetching name of the push url. This will make it so that repo's which
    have the name of the remote other than origin will be recognized as
    well.

--- lua/github_link/git.lua
+++ lua/github_link/git.lua
@@ -1,6 +1,6 @@
 -- Module to run various git commands in a seperate process and return the output.
 
-local Job = require("plenary.job")
+local Job = require('plenary.job')
 local M = {}
 
 -- Run git commands in a new process and return the output.
@@ -15,28 +15,34 @@ local M = {}
 --   Defaults to working directory of the buffer if not specified.
 M.git_cmd = function(args, cwd)
   return Job:new({
-    command = "git",
+    command = 'git',
     args = args,
-    cwd = cwd or vim.fn.expand("%:p:h"),
+    cwd = cwd or vim.fn.expand('%:p:h'),
     on_exit = function(j_self, _, _)
-      return table.concat(j_self:result(), "")
+      return table.concat(j_self:result(), '')
     end,
   }):sync()
 end
 
 -- Get the current branch name, this will be used to generate the blob for that branch.
 M.get_git_branch = function(cwd)
-  return M.git_cmd({ "branch", "--show-current" }, cwd)[1]
+  return M.git_cmd({ 'branch', '--show-current' }, cwd)[1]
 end
 
 -- Get the current git remote url.
 M.get_repo_url = function(cwd)
-  return M.git_cmd({ "config", "--get", "remote.origin.url" }, cwd)[1]
+  local remote = M.git_cmd({ 'remote', '-v' })[2]
+  if remote == nil then
+    return nil
+  end
+
+  remote = remote:gmatch('[^%s]+')
+  return M.git_cmd({ 'config', '--get', ('remote.%s.url'):format(remote) }, cwd)[1]
 end
 
 -- Get the root directory of the git repo.
 M.get_git_root = function(cwd)
-  return M.git_cmd({ "rev-parse", "--show-toplevel" }, cwd)[1]
+  return M.git_cmd({ 'rev-parse', '--show-toplevel' }, cwd)[1]
 end
 
 -- Get the relative path of the file from the git root.
@@ -49,7 +55,7 @@ end
 -- cwd: str | nil
 -- The working directory in which the command is to be executed.
 M.get_relative_path = function(file_path, cwd)
-  return M.git_cmd({ "ls-files", "--full-name", "--", file_path or vim.fn.expand("%:p") }, cwd)[1]
+  return M.git_cmd({ 'ls-files', '--full-name', '--', file_path or vim.fn.expand('%:p') }, cwd)[1]
 end
 
 return M
